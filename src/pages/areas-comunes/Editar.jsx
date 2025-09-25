3393c56528cdaed007e2c593b27adf05fe62bb1a
Alex Orellana <lexorel88@gmail.com>
2025-09-25 00:17:21 -0400

    Se mejoro crear editar listar ver( ver tiene dos opciones modal o componente)

--- src/pages/areas-comunes/Editar.jsx
+++ src/pages/areas-comunes/Editar.jsx
@@ -1,74 +1,233 @@
-import React, { useEffect, useState } from "react";
-import { Form, Button, Spinner } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Form, Button, Row, Col, Spinner } from "react-bootstrap";
+import { useParams, useNavigate } from "react-router-dom";
 import { fetchAreaComun, updateAreaComun } from "../../services/areas-comunes";
+import { listarCondominios } from "../../services/condominios";
 import DashboardLayout from "../../components/DashboardLayout";
-import { useNavigate, useParams } from "react-router-dom";
 
 function Editar() {
   const { id } = useParams();
   const navigate = useNavigate();
-  const [form, setForm] = useState(null);
+
+  const [form, setForm] = useState({
+    nombre: "",
+    descripcion: "",
+    capacidad: "",
+    horario_apertura: "",
+    horario_cierre: "",
+    precio_por_hora: "",
+    reglas_uso: "",
+    requiere_aprobacion: false,
+    condominio: "",
+  });
+
+  const [condominios, setCondominios] = useState([]);
+  const [loading, setLoading] = useState(true);
   const [saving, setSaving] = useState(false);
 
   useEffect(() => {
     loadData();
-  }, [id]);
+  }, []);
 
   const loadData = async () => {
     try {
-      const data = await fetchAreaComun(id);
+      const [area, condoData] = await Promise.all([
+        fetchAreaComun(id),
+        listarCondominios(),
+      ]);
+      console.log("Detalle Area:", area);
       setForm({
-        ...data,
-        condominio_id: data.condominio.id,
+        nombre: area.nombre,
+        descripcion: area.descripcion || "",
+        capacidad: area.capacidad || "",
+        horario_apertura: area.horario_apertura || "",
+        horario_cierre: area.horario_cierre || "",
+        precio_por_hora: area.precio_por_hora || "",
+        reglas_uso: area.reglas_uso || "",
+        requiere_aprobacion: area.requiere_aprobacion,
+        condominio: area.condominio?.id || "", //aseguramos id
       });
+
+      setCondominios(condoData.results || condoData);
     } catch (err) {
-      console.error("Error al cargar área común:", err);
+      console.error("Error cargando datos:", err);
+      alert("Ocurrió un error al cargar los datos.");
+    } finally {
+      setLoading(false);
     }
   };
 
   const handleChange = (e) => {
     const { name, value, type, checked } = e.target;
-    setForm({ ...form, [name]: type === "checkbox" ? checked : value });
+    setForm((prev) => ({
+      ...prev,
+      [name]: type === "checkbox" ? checked : value,
+    }));
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       setSaving(true);
-      await updateAreaComun(id, form);
+
+      const payload = {
+        ...form,
+        capacidad: parseInt(form.capacidad, 10),
+        precio_por_hora: parseFloat(form.precio_por_hora),
+        condominio_id: parseInt(form.condominio, 10),
+      };
+
+      delete payload.condominio; // eliminar campo no necesario
+
+      await updateAreaComun(id, payload);
       navigate("/areas-comunes");
     } catch (err) {
-      console.error("Error al guardar:", err);
-      alert("Error al actualizar el área común");
+      console.error("Error al actualizar área común:", err);
+      console.error("Respuesta del backend:", err.response?.data);
+      alert("Ocurrió un error al guardar");
     } finally {
       setSaving(false);
     }
   };
 
-  if (!form)
+  if (loading) {
     return (
       <DashboardLayout>
         <Spinner animation="border" />
       </DashboardLayout>
     );
+  }
 
   return (
     <DashboardLayout>
       <h2>Editar Área Común</h2>
       <Form onSubmit={handleSubmit}>
-        <Form.Group className="mb-3">
-          <Form.Label>Nombre</Form.Label>
-          <Form.Control
-            name="nombre"
-            value={form.nombre}
-            onChange={handleChange}
-            required
-          />
-        </Form.Group>
-        {/* Resto igual a Crear.jsx */}
-        {/* Puedes reutilizar el mismo formulario si lo separas en un componente FormAreaComun */}
-        <Button type="submit" disabled={saving}>
-          {saving ? "Guardando..." : "Guardar"}
+        {/* Línea 1 */}
+        <Row className="mb-3">
+          <Col md={6}>
+            <Form.Group>
+              <Form.Label>Nombre</Form.Label>
+              <Form.Control
+                name="nombre"
+                value={form.nombre}
+                onChange={handleChange}
+                required
+              />
+            </Form.Group>
+          </Col>
+          <Col md={6}>
+            <Form.Group>
+              <Form.Label>Descripción</Form.Label>
+              <Form.Control
+                name="descripcion"
+                value={form.descripcion}
+                onChange={handleChange}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+        {/* Línea 2 */}
+        <Row className="mb-3">
+          <Col md={4}>
+            <Form.Group>
+              <Form.Label>Capacidad</Form.Label>
+              <Form.Control
+                name="capacidad"
+                type="number"
+                min={0}
+                value={form.capacidad}
+                onChange={handleChange}
+                required
+              />
+            </Form.Group>
+          </Col>
+          <Col md={4}>
+            <Form.Group>
+              <Form.Label>Horario Apertura</Form.Label>
+              <Form.Control
+                name="horario_apertura"
+                type="time"
+                value={form.horario_apertura}
+                onChange={handleChange}
+                required
+              />
+            </Form.Group>
+          </Col>
+          <Col md={4}>
+            <Form.Group>
+              <Form.Label>Horario Cierre</Form.Label>
+              <Form.Control
+                name="horario_cierre"
+                type="time"
+                value={form.horario_cierre}
+                onChange={handleChange}
+                required
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+        {/* Línea 3 */}
+        <Row className="mb-3">
+          <Col md={4}>
+            <Form.Group>
+              <Form.Label>Precio por hora</Form.Label>
+              <Form.Control
+                name="precio_por_hora"
+                type="number"
+                step="0.01"
+                min={0}
+                value={form.precio_por_hora}
+                onChange={handleChange}
+                required
+              />
+            </Form.Group>
+          </Col>
+          <Col md={8}>
+            <Form.Group>
+              <Form.Label>Reglas de uso</Form.Label>
+              <Form.Control
+                name="reglas_uso"
+                value={form.reglas_uso}
+                onChange={handleChange}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+        {/* Línea 4 */}
+        <Row className="mb-3">
+          <Col md={6}>
+            <Form.Group>
+              <Form.Label>Condominio</Form.Label>
+              <Form.Select
+                name="condominio"
+                value={form.condominio}
+                onChange={handleChange}
+                required
+              >
+                <option value="">Seleccione un condominio</option>
+                {condominios.map((condo) => (
+                  <option key={condo.id} value={condo.id}>
+                    {condo.nombre}
+                  </option>
+                ))}
+              </Form.Select>
+            </Form.Group>
+          </Col>
+          <Col md={6} className="d-flex align-items-center">
+            <Form.Group>
+              <Form.Check
+                type="switch"
+                id="requiere-aprobacion-switch"
+                label="Requiere aprobación"
+                name="requiere_aprobacion"
+                checked={form.requiere_aprobacion}
+                onChange={handleChange}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Button variant="primary" type="submit" disabled={saving}>
+          {saving ? "Guardando..." : "Guardar cambios"}
         </Button>{" "}
         <Button variant="secondary" onClick={() => navigate("/areas-comunes")}>
           Cancelar
--- src/pages/areas-comunes/Listar.jsx
+++ src/pages/areas-comunes/Listar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Table, Button, Spinner } from "react-bootstrap";
+import { Table, Button, Spinner, Modal, Card } from "react-bootstrap";
 import {
   fetchAreasComunes,
   deleteAreaComun,
@@ -7,21 +7,40 @@ import {
 import DashboardLayout from "../../components/DashboardLayout";
 import { useNavigate } from "react-router-dom";
 
+import {
+  FaClock,
+  FaUsers,
+  FaMoneyBillWave,
+  FaCheckCircle,
+  FaTimesCircle,
+  FaHome,
+  FaListAlt,
+} from "react-icons/fa";
+
 function Listar() {
   const [areas, setAreas] = useState([]);
   const [loading, setLoading] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  // Estado para el modal de confirmación
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [selectedAreaId, setSelectedAreaId] = useState(null);
+
+  const [showModal, setShowModal] = useState(false);
+  const [selectedArea, setSelectedArea] = useState(null);
+
   const navigate = useNavigate();
 
   useEffect(() => {
-    loadAreas(currentPage);
-  }, [currentPage]);
+    loadAreas(currentPage, searchTerm);
+  }, [currentPage, searchTerm]);
 
-  const loadAreas = async (page) => {
+  const loadAreas = async (page, search = "") => {
     try {
       setLoading(true);
-      const data = await fetchAreasComunes(page);
+      const data = await fetchAreasComunes(page, search);
       setAreas(data.results);
       const total = Math.ceil(data.count / 10);
       setTotalPages(total);
@@ -32,19 +51,42 @@ function Listar() {
     }
   };
 
-  const handleDelete = async (id) => {
-    if (
-      !window.confirm("¿Estás seguro de que quieres eliminar esta área común?")
-    )
-      return;
+  // Modal handlers
+  const handleShowDeleteModal = (id) => {
+    setSelectedAreaId(id);
+    setShowDeleteModal(true);
+  };
+
+  const handleCloseDeleteModal = () => {
+    setSelectedAreaId(null);
+    setShowDeleteModal(false);
+  };
+
+  const confirmDelete = async () => {
     try {
-      await deleteAreaComun(id);
-      loadAreas(currentPage);
+      await deleteAreaComun(selectedAreaId);
+      loadAreas(currentPage, searchTerm); // Refrescar la lista
     } catch (err) {
       console.error("Error eliminando:", err);
       alert("Error al eliminar");
+    } finally {
+      handleCloseDeleteModal();
     }
   };
+
+  const handleView = (area) => {
+    setSelectedArea(area);
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setSelectedArea(null);
+  };
+
+  /*const filteredAreas = areas.filter((a) =>
+    `${a.nombre}`.toLowerCase().includes(searchTerm.toLowerCase())
+  );*/
 
   return (
     <DashboardLayout>
@@ -58,6 +100,19 @@ function Listar() {
         </Button>
       </div>
 
+      <div className="mb-3">
+        <input
+          type="text"
+          placeholder="Buscar por nombre, capacidad"
+          className="form-control"
+          value={searchTerm}
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+            setCurrentPage(1); // Reiniciar a la página 1 al buscar
+          }}
+        />
+      </div>
+
       {loading ? (
         <Spinner animation="border" />
       ) : (
@@ -65,6 +120,7 @@ function Listar() {
           <Table bordered hover responsive>
             <thead>
               <tr>
+                <th>Id</th>
                 <th>Nombre</th>
                 <th>Capacidad</th>
                 <th>Horario</th>
@@ -75,6 +131,7 @@ function Listar() {
             <tbody>
               {areas.map((area) => (
                 <tr key={area.id}>
+                  <td>{area.id}</td>
                   <td>{area.nombre}</td>
                   <td>{area.capacidad}</td>
                   <td>
@@ -82,6 +139,21 @@ function Listar() {
                   </td>
                   <td>{area.precio_por_hora}</td>
                   <td>
+                    <Button
+                      size="sm"
+                      variant="info"
+                      onClick={() => handleView(area)}
+                    >
+                      Ver
+                    </Button>{" "}
+                    {/*<Button
+                      variant="info"
+                      size="sm"
+                      className="me-2"
+                      onClick={() => navigate(`/areas-comunes/${area.id}`)}
+                    >
+                      Ver
+                    </Button>*/}
                     <Button
                       size="sm"
                       onClick={() =>
@@ -93,7 +165,7 @@ function Listar() {
                     <Button
                       size="sm"
                       variant="danger"
-                      onClick={() => handleDelete(area.id)}
+                      onClick={() => handleShowDeleteModal(area.id)}
                     >
                       Eliminar
                     </Button>
@@ -102,6 +174,7 @@ function Listar() {
               ))}
             </tbody>
           </Table>
+
           {totalPages > 1 && (
             <div className="d-flex justify-content-between align-items-center mt-3">
               <span>
@@ -128,6 +201,89 @@ function Listar() {
             </div>
           )}
         </>
+      )}
+      {/* Modal de confirmación */}
+      <Modal show={showDeleteModal} onHide={handleCloseDeleteModal} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Confirmar Eliminación</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          ¿Estás seguro de que quieres eliminar esta área común?
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseDeleteModal}>
+            Cancelar
+          </Button>
+          <Button variant="danger" onClick={confirmDelete}>
+            Eliminar
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      {selectedArea && (
+        <Modal show={showModal} onHide={handleCloseModal} centered size="lg">
+          <Modal.Header closeButton>
+            <Modal.Title>
+              Detalles de Área Común: <strong>{selectedArea.nombre}</strong>
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Card className="shadow-sm border-0">
+              <Card.Body>
+                <p>
+                  <FaListAlt className="me-2 text-primary" />{" "}
+                  <strong>Descripción:</strong> {selectedArea.descripcion}
+                </p>
+                <hr />
+                <p>
+                  <FaUsers className="me-2 text-success" />{" "}
+                  <strong>Capacidad:</strong> {selectedArea.capacidad}
+                </p>
+                <p>
+                  <FaClock className="me-2 text-warning" />
+                  <strong>Horario:</strong> {selectedArea.horario_apertura} -{" "}
+                  {selectedArea.horario_cierre}
+                </p>
+                <hr />
+                <p>
+                  <FaMoneyBillWave className="me-2 text-info" />{" "}
+                  <strong>Precio por hora:</strong> $
+                  {selectedArea.precio_por_hora}
+                </p>
+                <p>
+                  <FaListAlt className="me-2 text-secondary" />{" "}
+                  <strong>Reglas de uso:</strong> {selectedArea.reglas_uso}
+                </p>
+                <hr />
+                <p>
+                  {selectedArea.requiere_aprobacion ? (
+                    <>
+                      <FaCheckCircle className="me-2 text-success" />{" "}
+                      <strong>Requiere aprobación:</strong> Sí
+                    </>
+                  ) : (
+                    <>
+                      <FaTimesCircle className="me-2 text-danger" />{" "}
+                      <strong>Requiere aprobación:</strong> No
+                    </>
+                  )}
+                </p>
+                {selectedArea.condominio?.nombre && (
+                  <p>
+                    <FaHome className="me-2 text-dark" />{" "}
+                    <strong>Condominio:</strong>{" "}
+                    {selectedArea.condominio.nombre}
+                  </p>
+                )}
+              </Card.Body>
+            </Card>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleCloseModal}>
+              Volver a la lista
+            </Button>
+          </Modal.Footer>
+        </Modal>
       )}
     </DashboardLayout>
   );
